9ae41f5e949d79ac8a4474f595d91992fc313bb0
shivamkrs89 <70107607+shivamkrs89@users.noreply.github.com>
2021-03-19 18:38:16 +0530

    Update Trapping_rain_water.cpp

--- Trapping_rain_water.cpp
+++ Trapping_rain_water.cpp
@@ -181,3 +181,43 @@ int main(){
     
     return 0;
 }  // } Driver Code Ends
+
+
+Alternate easy way
+//C++
+
+int trappingWater(int arr[], int n){
+   
+    // left[i] contains height of tallest bar to the 
+    // left of i'th bar including itself 
+    vector<int> left(n, 0);
+  
+    // Right [i] contains height of tallest bar to 
+    // the right of ith bar including itself 
+    vector<int> right(n, 0); 
+  
+    // Initialize result 
+    int water = 0;
+  
+    // Fill left array 
+    left[0] = arr[0];
+    for (int i = 1;i < n;i++) {
+        left[i] = max(left[i - 1], arr[i]);
+    }
+  
+    // Fill right array 
+    right[n-1] = arr[n-1]; 
+    for (int i = n - 2;i >= 0;i--) {
+        right[i] = max(right[i + 1], arr[i]);
+    }
+  
+    // Calculate the accumulated water element by element 
+    // consider the amount of water on i'th bar, the 
+    // amount of water accumulated on this particular 
+    // bar will be equal to min(left[i], right[i]) - arr[i] . 
+    for (int i = 0;i < n;i++) {
+        water += max(0, min(left[i], right[i]) - arr[i]);
+    }
+  
+    return water;
+}
