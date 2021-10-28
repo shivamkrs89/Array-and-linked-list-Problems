2a8b0cc3460368a5b60dfa1b5a1452b910cda707
shivamkrs89 <70107607+shivamkrs89@users.noreply.github.com>
2021-10-28 15:16:52 +0530

    Create interleaved_linked_list.cpp

---
+++ interleaved_linked_list.cpp
@@ -0,0 +1,53 @@
+Given two linked lists l0 and l1, return the two linked lists interleaved, starting with l0. If there are leftover nodes in a linked list, they should be added to the end.
+
+Constraints
+
+    n ≤ 100,000 where n is the number of nodes in l0
+    m ≤ 100,000 where m is the number of nodes in l1
+
+Example 1
+Input
+Visualize
+
+l0 =
+
+7
+
+8
+
+l1 =
+
+1
+
+2
+Output
+Visualize
+
+7
+
+1
+
+8
+
+2
+Example 2
+Input
+Visualize
+
+l0 =
+
+7
+
+8
+
+l1 =
+
+1
+Output
+Visualize
+
+7
+
+1
+
+8
